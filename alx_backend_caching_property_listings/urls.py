2c7e64cd85e09799a24ca19cb5a54b437a0dd0bf
Johnson Oyemade <johnsonoca@gmail.com>
2026-01-11 23:33:51 +0100

    Cache Property List View

--- alx_backend_caching_property_listings/urls.py
+++ alx_backend_caching_property_listings/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # Import 'include'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Route requests starting with 'properties/' to the properties app
+    path('properties/', include('properties.urls')),
 ]
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+# Cache the response for 15 minutes (60 seconds * 15)
+@cache_page(60 * 15)
+def property_list(request):
+    # Get all properties from the database
+    properties = Property.objects.all()
+    
+    # Convert query set to a list of dictionaries so we can return JSON
+    # (If you were using templates, you would just pass 'properties' in the context)
+    properties_list = list(properties.values('title', 'price', 'location', 'description'))
+    
+    return JsonResponse(properties_list, safe=False)
